d9a21095deb7b8d4a504466375aafe1251a69cc1
naorlivne <naorlivne@gmail.com>
2019-11-25 13:10:24 +0200

    updating to match the new terraform 0.12.x syntax

--- nebula-packet-global-deployment-example/variables.tf
+++ nebula-packet-global-deployment-example/variables.tf
@@ -1,24 +1,24 @@
 # the server size you want
 variable "server_size" {
-  type = "string"
+  type = string
   default = "c1.small.x86"
 }
 
 # the region where server devices will be deployed
 variable "server_region_device" {
-  type = "string"
+  type = string
   default = "ams1"
 }
 
 # the count of how many servers you want
 variable "worker_count" {
-  type = "string"
+  type = string
   default = 5
 }
 
 # a list of regions where devices will be deployed
 variable "worker_region_device" {
-  type = "list"
+  type = list(any)
   default = [
     "ams1",
     "dfw2",
@@ -30,6 +30,6 @@ variable "worker_region_device" {
 
 # the server size you want
 variable "worker_size" {
-  type = "string"
+  type = string
   default = "c1.small.x86"
 }
